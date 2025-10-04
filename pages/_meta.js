cfe4037db4b88d0a36805897a4158adaf1ca1510
mahek <mahekzehra5@gmail.com>
2025-10-04 06:55:28 -0700

    project setup done

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,20 +1,15 @@
 export default {
-  "index": "Introduction",
-    "gripo": {
-    "title": "Gripo home",
-    "type": "page",
-    "href": "https://www.gripo.io",
-    "newWindow": true
-  },
-  "contact": {
-    "title": "Contact ↗",
-    "type": "page",
-    "href": "https://www.gripo.io/contact",
-    "newWindow": true
+  index: "Introduction",
+
+  contact: {
+    title: "Contact ↗",
+    type: "page",
+    href: "https://www.gripo.io/contact",
+    newWindow: true,
   },
   "request-demo": {
-    "title": "Request free license 🔥",
-    "type": "page",
-    "href": "https://www.gripo.io/request-free-license"
-  }
+    title: "Request free license 🔥",
+    type: "page",
+    href: "https://www.gripo.io/request-free-license",
+  },
 };
